1790c12bd7c6cf7b5795cd030d0e1434b051b61b
Sébastien Mosser <mosser@i3s.unice.fr>
2018-02-18 20:22:32 -0400

    csv and feature files can be specified as command line arguments

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,34 +1,43 @@
 import fr.familiar.interpreter.FMLShell;
-import fr.familiar.parser.FMBuilder;
-import fr.familiar.parser.FMLCommandInterpreter;
 import fr.familiar.variable.FeatureModelVariable;
-import io.CSVReader;
-import io.ConfigFile;
-import io.ToFamiliar;
-import sun.util.resources.cldr.en.CalendarData_en_CA;
+import io.*;
+import org.apache.commons.cli.*;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.Map;
 import java.util.Set;
 
 public class Main {
 
+    private static CommandLine arguments;
+
     // start with (mvn -q exec:java)
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Composition Catalogue program");
+        readParameters(args);
 
         System.out.println("Reading catalogue CSV file");
-        CSVReader reader = new CSVReader();
+        String productFile = arguments.getOptionValue("csv");
+        CSVReader reader = ( productFile == null? new CSVReader() : new CSVReader(new File(productFile)));
         Set<Map<String, String>> rawData = reader.contents();
 
         System.out.println("Transforming each line into individual products");
-        ConfigFile configuration = new ConfigFile();
+        String featureFile = arguments.getOptionValue("features");
+        ConfigFile configuration = (featureFile == null? new ConfigFile(): new ConfigFile(new File(featureFile)));
         ToFamiliar transformer = new ToFamiliar(rawData, "Approach", configuration.criteria());
         Map<String, String> products = transformer.getProducts();
 
         System.out.println("Merging products into a single catalogue");
         FeatureModelVariable catalogue = buildCatalogue(products);
         System.out.println("Number of products: " + catalogue.counting());
+    }
+
+    private static void readParameters(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption("csv", true, "CSV file containing the products");
+        options.addOption("features", true, "TXT file containing the feature descriptions");
+        CommandLineParser parser = new DefaultParser();
+        arguments = parser.parse( options, args);
     }
 
     private static FeatureModelVariable buildCatalogue(Map<String, String> products) throws Exception {
